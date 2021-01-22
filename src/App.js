bbfc471953c028241cd9640d6f71fb21ba12145a
vijay300 <gnvijayk@gmail.com>
2021-01-22 15:45:55 +0530

    [Vijay] Added: onclick function to launch the bridze lab site in new tab when the image is clicked

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import logo from '../src/assets/images/Logo.svg';
 
 class App extends React.Component {
 
+  url = 'https://www.bridgelabz.com/'
   constructor() {
     super()
     this.state = {
       title: 'Hello from Bridgelabz'
     }
   }
+
+  onClick = (event) => {
+    console.log("image is clicked!", event);
+    window.open(this.url, "blank");
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="The Brodgelabz logo"/>
+        <img src={logo} onClick={this.onClick}
+          alt="The Brodgelabz logo"/>
       </div>
     );
   }
