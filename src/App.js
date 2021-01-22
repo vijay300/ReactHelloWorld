8734febb1b69586645c0fb010f5c3e345f2d4871
vijay300 <gnvijayk@gmail.com>
2021-01-22 15:33:17 +0530

    [Vijay] Refactored: for one way binding using template expression to display the data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      title: 'Hello from Bridgelabz'
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Hello from Bridgelabz</h1>
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
